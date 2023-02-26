1272bcb788c0fa2576a96bf826c21b75ca19b501
Chris Xu <xuchen@jiazhiyi.com>
2023-02-26 14:14:34 +0800

    Initial commit

---
+++ src/main/java/com/learning/hr_sys/HrSysApplication.java
@@ -0,0 +1,13 @@
+package com.learning.hr_sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrSysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learning/hr_sys/HrSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.hr_sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
